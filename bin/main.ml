8f6b2f3caf7d784f26079bcb7bb4a1bfd9c2c4b8
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-11-12 06:05:24 +0100

    Pass filter on cmdline

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,13 @@
 open Jaq
 
-let () = Read.read_stdin () |> json_pp |> print_endline
+let filter_str =
+  match Array.length Sys.argv with
+  | 1 -> None
+  | 2 -> Some Sys.argv.(1)
+  | _ -> failwith "Incorrect number of arguments"
+
+let filter f j = match f with None -> j | Some f -> Filter.exec f j
+
+let () =
+  Read.read_stdin () |> parse_json |> filter filter_str |> json_pp
+  |> print_endline
--- lib/jaq.ml
+++ lib/jaq.ml
@@ -1,7 +1,6 @@
 module Read = Read
 
-let json json_string = Yojson.Safe.from_string json_string
+let parse_json json_string = Yojson.Safe.from_string json_string
 (* val json : Yojson.Safe.t *)
 
-let json_pp json_string =
-  Yojson.Safe.pretty_to_string ~std:false @@ json json_string
+let json_pp json = Yojson.Safe.pretty_to_string ~std:false json
