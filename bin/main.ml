b0254b26c9e1a8bc4ee52435d25d3cd26ba22e4a
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-11-19 21:44:26 +0100

    Add help message

--- bin/main.ml
+++ bin/main.ml
@@ -11,6 +11,21 @@ let args =
 
 let filter f j = match f with None | Some "" -> j | Some f -> Filter.exec f j
 
+let help_msg =
+  {|jaq - json processing tool with sane filter language
+
+USAGE: jaq FILTER <INPUT>
+
+if no input file is provided, stdin will be read|}
+
+let help () =
+  match Sys.argv.(1) with
+  | "-h" | "--help" ->
+      print_endline help_msg;
+      exit 1
+  | (exception Invalid_argument _) | _ -> ()
+
 let () =
+  help ();
   Read.read args.file |> parse_json |> filter args.filter |> json_pp
   |> Print.print
