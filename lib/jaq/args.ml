6448f735f3e3a4d4a7d2cf4fad7e259731b3a75e
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-12-06 23:26:38 +0100

    Add extract flag

--- lib/jaq/args.ml
+++ lib/jaq/args.ml
@@ -1,18 +1,18 @@
-type args = { filter : string option; file : string option }
+type args = { filter : string option; file : string option; extract : bool }
 
-let args filter_file anons =
+let args extract filter_file anons =
   match filter_file with
   | "" -> (
       match anons with
-      | [] -> { filter = None; file = None }
-      | [ filter ] -> { filter = Some filter; file = None }
-      | [ filter; file ] -> { filter = Some filter; file = Some file }
+      | [] -> { filter = None; file = None; extract = extract}
+      | [ filter ] -> { filter = Some filter; file = None; extract = extract }
+      | [ filter; file ] -> { filter = Some filter; file = Some file; extract = extract }
       | _ -> Error.exit_with_msg "Incorrect number of arguments")
   | ff -> (
       let filter = Read.read (Some ff) in
       match anons with
-      | [] -> { filter = Some filter; file = None }
-      | [ file ] -> { filter = Some filter; file = Some file }
+      | [] -> { filter = Some filter; file = None; extract = extract }
+      | [ file ] -> { filter = Some filter; file = Some file; extract = extract }
       | _ -> Error.exit_with_msg "Incorrect number of arguments")
 
 let help_msg =
@@ -27,6 +27,7 @@ ARGS:
 OPTIONS:
   -h, --help         print this message and exit
   -f, --from-file    read filter from file 
+  -e, --extract      extract plain-text value from result json
 
 |}
 
@@ -37,10 +38,14 @@ let print_help () =
 let apply_optional f s = match f with Some f -> f s | None -> ()
 
 let parse_args () =
-  let anons = ref [] and filter_file = ref "" in
+  let anons = ref [] 
+  and filter_file = ref "" 
+  and extract = ref false
+  in
   let specs =
     [
       ('h', "help", Some print_help, None);
+      ('e', "extract", (Getopt.set extract true), None);
       ( 'f',
         "from-file",
         None,
@@ -49,4 +54,4 @@ let parse_args () =
     ]
   in
   Getopt.parse_cmdline specs (apply_optional @@ Getopt.append anons);
-  args !filter_file !anons
+  args !extract !filter_file !anons
