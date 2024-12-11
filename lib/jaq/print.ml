583cc888e1aec4150287f1476188906294991c89
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-12-11 04:52:38 +0100

    Use char list instead of strings in colorize for the input string

--- lib/jaq/print.ml
+++ lib/jaq/print.ml
@@ -3,39 +3,42 @@ let append_flush = Fun.flip ( ^ ) "\n%!"
 type state = InKey | PostKey | InVal | Neutral
 type ctx = Start | InArray | InObj
 
-let str_head = function "" -> None | s -> Some (String.sub s 0 1)
-let str_tail = function "" -> "" | s -> String.sub s 1 (String.length s - 1)
+let explode s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
 
-let should_switch_to_neutral str state =
-  String.contains ",{}[" (String.get str 0) && state = PostKey
+let suffix_char s c = s ^ String.make 1 c
 
-let update_ctx_stack stack state s =
+let should_switch_to_neutral c state =
+  String.contains ",{}[" c && state = PostKey
+
+let update_ctx_stack stack state c =
   match state with
   | Neutral | PostKey -> (
-      match s with
-      | "{" -> InObj :: stack
-      | "[" -> InArray :: stack
-      | "}" | "]" -> List.tl stack
+      match c with
+      | '{' -> InObj :: stack
+      | '[' -> InArray :: stack
+      | '}' | ']' -> List.tl stack
       | _ -> stack)
   | _ -> stack
 
 let rec colorize' ctx_stack state acc raw =
-  let rest = str_tail raw in
-  match str_head raw with
-  | None -> acc
-  | Some "\"" -> (
+  match raw with
+  | [] -> acc
+  | '"' :: rest -> (
       match (state, List.hd ctx_stack) with
       | Neutral, InArray | PostKey, _ ->
           colorize' ctx_stack InVal (acc ^ "@{<green>\"") rest
       | Neutral, _ -> colorize' ctx_stack InKey (acc ^ "@{<blue>\"") rest
       | InKey, _ -> colorize' ctx_stack PostKey (acc ^ "\"@}") rest
       | InVal, _ -> colorize' ctx_stack Neutral (acc ^ "\"@}") rest)
-  | Some s when should_switch_to_neutral s state ->
-      colorize' (update_ctx_stack ctx_stack state s) Neutral (acc ^ s) rest
-  | Some s ->
-      colorize' (update_ctx_stack ctx_stack state s) state (acc ^ s) rest
+  | c :: rest when should_switch_to_neutral c state ->
+      colorize' (update_ctx_stack ctx_stack state c) Neutral (suffix_char acc c) rest
+  | c :: rest ->
+      colorize' (update_ctx_stack ctx_stack state c) state (suffix_char acc c) rest
 
-let colorize = colorize' [ Start ] Neutral ""
+let colorize raw = colorize' [ Start ] Neutral "" (explode raw)
 
 let print raw =
   if Unix.isatty Unix.stdout then
