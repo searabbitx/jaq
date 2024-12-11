bdc91dc1eb7909be0248a70772c63d144c876e63
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-12-11 05:11:12 +0100

    Remove time functions

--- bin/main.ml
+++ bin/main.ml
@@ -7,9 +7,5 @@ let () =
   let print_fun =
     if args.extract then Print.extract else Util.comp Print.print json_pp
   in
-  try
-    Read.read args.file
-    |> Jaq.Time.time1 "parse_json" parse_json
-    |> Jaq.Time.time1 "filter" (filter args.filter)
-    |> Jaq.Time.time1 "print_fun" print_fun
+  try Read.read args.file |> parse_json |> filter args.filter |> print_fun
   with e -> Error.handle_error e
